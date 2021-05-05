28fe777f15e3dff6a1bc972ac61866b9545b41aa
mahesh9022 <mv8592203@gmail.com>
2021-05-05 11:50:00 +0530

    init commit

---
+++ Api2cosmos/Function1.cs
@@ -0,0 +1,58 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.Web.Helpers;
+
+namespace Api2cosmos
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB(
+        databaseName: "",
+        collectionName: "",
+        ConnectionStringSetting = "CosmosDbConnectionString")]IAsyncCollector<dynamic> documentsOut,
+        ILogger log)
+        {
+            string rvalue = "ok";
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            using (var client = new HttpClient())
+            {
+               
+                //HTTP GET
+                var responseTask = client.GetAsync("https://byotdms-website.azurewebsites.net/api/master/GetModels?date=1900-10-18 04:53:13.684&start=0&pageSize=500");
+                responseTask.Wait();
+                
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    rvalue = result.Content.ReadAsStringAsync().Result;
+                    dynamic jsonval = JsonConvert.DeserializeObject<dynamic>(rvalue);
+                    
+                    foreach (dynamic element in jsonval.ResponseObject)
+                    {
+                        var temp = element;
+                        var temp2 = element.id;
+                        element.id= element.id.ToString();
+                        await documentsOut.AddAsync(element);
+                    }
+
+                }
+            }
+
+            return new OkObjectResult(rvalue);
+        }
+    }
+}
